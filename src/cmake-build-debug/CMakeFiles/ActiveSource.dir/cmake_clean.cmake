ab592da56226856ee6e4731295da7331830532c3
pecacho214 <nickcampos06@gmail.com>
2024-04-01 12:15:00 -0400

    Initial Commit - Created main and set up CMake to use SFML

---
+++ src/cmake-build-debug/CMakeFiles/ActiveSource.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "ActiveSource"
+  "ActiveSource.pdb"
+  "CMakeFiles/ActiveSource.dir/main.cpp.o"
+  "CMakeFiles/ActiveSource.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ActiveSource.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/cmake-build-debug/CMakeFiles/ActiveSource.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ActiveSource.
+# This may be replaced when dependencies are built.
